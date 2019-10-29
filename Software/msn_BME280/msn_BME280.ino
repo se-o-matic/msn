26a6c3c1219365e760dd8ac3278602429f0589f5
se-o-matic <mail@s-engel.de>
2019-10-29 21:57:53 +0100

    change library for BME280. The new one features the forced mode for energy saving.

--- Software/msn_BME280/msn_BME280.ino
+++ Software/msn_BME280/msn_BME280.ino
@@ -1,58 +1,50 @@
 /*
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2018 Sensnology AB
- * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * DESCRIPTION
- *
- * This is an template for RFM69CW 433 MHz sensors, which run on two AA or AAA batterys
- *
+ * My Sensors Node for temperature and humidity
+ * 
+ * Hardware: MSN R00
+ * BME280 (temp, hum) at I2C
+ * 
+ * Runs at 2 x AA batterys
+ * 
  */
- 
-// 1M, 470K divider across battery and using internal ADC ref of 1.1V
-// Sense point is bypassed with 0.1 uF cap to reduce noise at that point
-// ((1e6+470e3)/470e3)*1.1 = Vmax = 3.44 Volts
-// 3.44/1023 = Volts per bit = 0.003363075
-// Enable and select radio type attached
+
+// RFM69CW at 433 MHz / Raspebrry Pi as ethernet gateway
 #define MY_RADIO_RFM69
 #define MY_RFM69_NEW_DRIVER
 #define MY_RFM69_FREQUENCY RFM69_433MHZ 
 #include <MySensors.h>
-#include "cactus_io_BME280_I2C.h"
- 
+
+// BME280 library from Adafruit, need the Adafruit "Unified Sensor library", too
+#include <Adafruit_BME280.h>
+
+// Define child IDs and messages
 #define CHILD_ID_TEMP  0
 #define CHILD_ID_HUM   1
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 
+// Define strings, which are send to the controller
 #define SKETCHSTR "Temp Humidity Sensor"
-#define SKETCHVER "0.1"
+#define SKETCHVER "0.2.0"
 
-BME280_I2C bme(0x76);
+// Sleep time for sensor 
 uint32_t SLEEP_TIME = 300000;    // sleep time between reads (seconds * 1000 milliseconds)
 
+// Constructor for sensor
+Adafruit_BME280 bme;
+#define BME280_TEMP_OFFSET -1.0    // calibration ofsset for the temperature
+
 // Battery stuff
-#define EMPTY 536         // Brown Out ATMega   //536 entspricht 1.8 V
-#define SCALE 0.2415      // (100/950-EMPTY)    //950 entspricht 3.2 V
+#define EMPTY 536         // Brown Out ATMega   //536 equal to 1.8 V
+#define SCALE 0.2415      // (100/950-EMPTY)    //950 equal to 3.2 V
 int BATTERY_SENSE_PIN = A0;   // select the input pin for the battery sense point
 int batteryPcnt = 0, oldBatteryPcnt = 0;
 
+// Global variables for sensor values
 float temp = 25.0, hum = 50.0;
 float temp_last = 0.0, hum_last = 0.0;
 
+// Helper for calculate battery percentage
 int calc_pcnt(int value){
   int batteryPcnt = ((value - EMPTY) * SCALE);
   batteryPcnt = (batteryPcnt > 100) ? 100 : batteryPcnt;
@@ -65,7 +57,12 @@ void setup() {
   analogReference(INTERNAL);
 
   //BME280 stuff
-  bme.begin();
+  bme.begin(0x77);
+  bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                  Adafruit_BME280::SAMPLING_X1,   // temperature
+                  Adafruit_BME280::SAMPLING_NONE, // pressure
+                  Adafruit_BME280::SAMPLING_X1,   // humidity
+                  Adafruit_BME280::FILTER_OFF );
 
   oldBatteryPcnt = calc_pcnt(analogRead(BATTERY_SENSE_PIN));
 }
@@ -80,14 +77,11 @@ void presentation()
 }
 
 void loop() {
-  //read temp and hum
-
-  static MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
-  static MyMessage msgHum(CHILD_ID_HUM, V_HUM);
-
-  bme.readSensor();
-  temp = bme.getTemperature_C();
-  hum = bme.getHumidity();
+  //read temp and hum and lux
+  bme.takeForcedMeasurement();
+  temp = bme.readTemperature();
+  temp = temp + BME280_TEMP_OFFSET;
+  hum = bme.readHumidity();
 
   if(temp != temp_last){
     send(msgTemp.set(temp, 2));
