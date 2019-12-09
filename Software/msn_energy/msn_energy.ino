b44b07e6addc66df293cc1032523c33fc3477caf
se-o-matic <mail@s-engel.de>
2019-12-09 19:57:17 +0100

    Some optimizations

--- Software/msn_energy/msn_energy.ino
+++ Software/msn_energy/msn_energy.ino
@@ -1,33 +1,13 @@
 /*
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2018 Sensnology AB
- * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * DESCRIPTION
- *
- * This is an template for RFM69CW 433 MHz sensors, which run on two AA or AAA batterys
+ * msn_energy.ino
+ * MySensors Node for reading actual kwh from an energy meter.
+ * 
+ * Hardware:
+ * msn board by SE
+ * IR head for serial communication with the meter.
  *
  */
- 
-// 1M, 470K divider across battery and using internal ADC ref of 1.1V
-// Sense point is bypassed with 0.1 uF cap to reduce noise at that point
-// ((1e6+470e3)/470e3)*1.1 = Vmax = 3.44 Volts
-// 3.44/1023 = Volts per bit = 0.003363075
+
 // Enable and select radio type attached
 #define MY_RADIO_RFM69
 #define MY_RFM69_NEW_DRIVER
@@ -38,16 +18,15 @@
 #define CHILD_ID_KWH   1
 
 #define SKETCHSTR "Energy Meter"
-#define SKETCHVER "0.1"
+#define SKETCHVER "0.1.1"
 
 uint32_t SLEEP_TIME = 1800000;    // sleep time between reads (seconds * 1000 milliseconds)
-//uint32_t SLEEP_TIME = 10000;
-#define PART_HOUR 2000               // 1/h * 1000 ==> kWh --> W
+#define PART_HOUR 2000            // 1/h * 1000 ==> kWh --> W
 
 // Battery stuff
-#define EMPTY 536         // Brown Out ATMega   //536 entspricht 1.8 V
-#define SCALE 0.2415      // (100/950-EMPTY)    //950 entspricht 3.2 V
-int BATTERY_SENSE_PIN = A0;   // select the input pin for the battery sense point
+#define EMPTY 536                 // Brown Out ATMega   //536 equal to 1.8 V
+#define SCALE 0.2415              // (100/950-EMPTY)    //950 equal to 3.2 V
+int BATTERY_SENSE_PIN = A0;       // input pin for battery sensing
 int batteryPcnt = 0;
 int oldBatteryPcnt = 0;
 
@@ -58,6 +37,10 @@ float energy = 0, energy_last = 0.0;
 float power = 0;
 
 int calc_pcnt(int value){
+  // 1M, 470K divider across battery and using internal ADC ref of 1.1V
+  // Sense point is bypassed with 0.1 uF cap to reduce noise at that point
+  // ((1e6+470e3)/470e3)*1.1 = Vmax = 3.44 Volts
+  // 3.44/1023 = Volts per bit = 0.003363075
   int batteryPcnt = ((value - EMPTY) * SCALE);
   batteryPcnt = (batteryPcnt > 100) ? 100 : batteryPcnt;
   batteryPcnt = (batteryPcnt < 0) ? 0 : batteryPcnt;
@@ -65,22 +48,20 @@ int calc_pcnt(int value){
 }
 
 float get_energy(void){
-  //static float add = 0.1;
   float kwh = 0.0;
   int timeout = 0;
   bool done = 0;
 
-  //Alte Daten aus dem Buffer abholen und ignorieren
+  //read old data and purge it
   while(Serial.available())Serial.read();
  
-  // Übertragung starten
-  Serial.write("/?!\r\n");
-  //Pafal sendet Ident "/PAF5EC3g00006\r\n"
+  //start reading with requesting the ident
+  Serial.write("/?!\r\n");        //meter sends ident "/PAF5EC3g00006\r\n"
   do{
     if (Serial.available() > 0){
       Serial.readBytesUntil('\n', buffer, sizeof(buffer));
       timeout = 0;
-       if(strncmp(buffer, "/PAF", 4) == 0) break;
+      if(strncmp(buffer, "/PAF", 4) == 0) break;
     } else {
       timeout++;
     }
@@ -93,7 +74,6 @@ float get_energy(void){
  
   //Pafal sendet alle Daten
   timeout = 0;
-
   do{
     if (Serial.available() < 1) timeout++;
     else {
@@ -113,8 +93,6 @@ float get_energy(void){
     }
     delay(1);
   } while (timeout < 5000 && !done);
-  //add += 0.1;
-  //float kwh = 1234.56 + add;
   return kwh;
 }
 
@@ -122,11 +100,10 @@ void setup() {
   // use the 1.1 V internal reference
   analogReference(INTERNAL);
 
-  Serial.begin(300, SERIAL_7E1);
+  Serial.begin(300, SERIAL_7E1);        // Meter uses 300 baud, 7 data bits, even parity, 1 stop bit
 
   //Wert hängt vom Zähler ab
   Serial.setTimeout(1000);    //1000 ms = default
-  //pinMode(LED_BUILTIN, OUTPUT);
 
   energy_last = get_energy();
   oldBatteryPcnt = calc_pcnt(analogRead(BATTERY_SENSE_PIN));
@@ -162,8 +139,10 @@ void loop() {
   static MyMessage msgEnergy(CHILD_ID_KWH, V_KWH);
 
   if(energy > 8000){
-    send(msgPower.set(power, 2));
     send(msgEnergy.set(energy, 2));
+  }
+  if(power > 0){
+    send(msgPower.set(power, 2));
   }
 
   // send the battery level
